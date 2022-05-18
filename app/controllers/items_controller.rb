80633747a5482f67c6edce62742a84739bafb1c3
Jbterrylin <wanglinlee1707@gmail.com>
2022-05-18 23:43:07 +0800

    add item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,41 @@ class ItemsController < ApplicationController
         item = Item.all
         render json: { status: :ok, data: item }
     end
+
+    def pagination
+        puts params
+        if(params[:page] & params[:results])
+            all = Item.where(users_id: helpers.get_user_id).where(is_deleted: [nil, false]).where.not(unit: [nil, ''])
+
+            # filter
+            if(params[:description])
+                all = all.where("description like ?", "%#{params[:description][0]}%")
+            end
+            if(params[:unit_price])
+                all = all.where("cast(unit_price as float) >= ? AND cast(unit_price as float) <= ?", params[:unit_price][0][0], params[:unit_price][0][1])
+            end
+            if(params[:unit])
+                all = all.where("cast(unit as float) >= ? AND cast(unit as float) <= ?", params[:unit][0][0], params[:unit][0][1])
+                puts all
+            end
+
+            all = all.includes(:paper)
+
+            items = nil
+            # sort
+            if(params[:sortField])
+                items = all.order(params[:sortField] + (params[:sortOrder] == "ascend" ? "" : " DESC")).first(params[:results] * params[:page]).last(10)
+            else
+                items = all.last(params[:results] * params[:page]).first(10).reverse()
+            end
+
+            puts "---"
+            unit_prices = Item.where(users_id: helpers.get_user_id).where(is_deleted: [nil, false]).where.not(unit: [nil, '']).map(&:unit_price).map(&:to_f)
+            units = Item.where(users_id: helpers.get_user_id).where(is_deleted: [nil, false]).where.not(unit: [nil, '']).map(&:unit).map(&:to_f)
+
+            render json: { status: :ok, data: items.as_json(include: :paper), detail: { total: all.count(), unit_price: [unit_prices.min, unit_prices.max], unit: [units.min, units.max] } }
+            return
+        end
+        render json: { errors: "don't have enough variable" }, status: 422
+    end
 end
